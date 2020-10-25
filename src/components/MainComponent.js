7a00392ab7d17c8a8fd939787dd604b6d50bd008
Christian Nesheim <cnesheim@gmail.com>
2020-10-25 15:21:58 -0500

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,12 +4,14 @@ import Footer from './FooterComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
-import Contact from './ContactComponent'
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+
 
 class Main extends Component {
     constructor(props) {
@@ -31,6 +33,15 @@ class Main extends Component {
                 partner={this.state.partners.filter(partner => partner.featured)[0]}
                 />
             );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
         };
 
         return (
@@ -39,7 +50,9 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
